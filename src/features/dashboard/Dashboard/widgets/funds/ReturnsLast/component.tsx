437bd88c281ff55c0e0f8e86d14460fe9f3641fe
nifanic <nifanic@masterpage.net>
2026-01-26 21:33:43 -0600

    refactor(ReturnsLast): :recycle: Add `subTitle`
    OPTA-21

--- src/features/dashboard/Dashboard/widgets/funds/ReturnsLast/component.tsx
+++ src/features/dashboard/Dashboard/widgets/funds/ReturnsLast/component.tsx
@@ -3,7 +3,7 @@ import { Widget, WidgetProps } from "@/features";
 
 export function ReturnsLast(props: Omit<WidgetProps, "title">) {
   return (
-    <Widget {...props} title="Returns">
+    <Widget {...props} title="Returns" subTitle="Last 12mo">
       <Placeholder sx={{ backgroundColor: "unset", flex: 1 }}>
         Returns last 12mo
       </Placeholder>
