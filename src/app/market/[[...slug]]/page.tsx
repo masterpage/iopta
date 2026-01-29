e2c47c66b0b605f51113619f4ed082152178680f
nifanic <nifanic@masterpage.net>
2026-01-28 18:57:56 -0600

    refactor(MarketPage): :recycle: Convert to dynamic route page
    OPTA-31

---
+++ src/app/market/[[...slug]]/page.tsx
@@ -0,0 +1,43 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+import { pages } from "@/consts";
+import { MarketType } from "@/features";
+import { getPageTitle } from "@/utils";
+
+import MarketPage from "./MarketPage";
+import { MarketPageProps } from "./types";
+
+const { label: MARKET } = pages.find((p) => p.uri === "/market") ?? {
+  label: "Unkn.",
+};
+
+export async function generateMetadata({
+  params,
+}: MarketPageProps): Promise<Metadata> {
+  const { slug = [] } = (await params) || {};
+  const [slugMarketType, ...restSlug] = slug.map((s) => s?.toLowerCase()) as [
+    MarketType,
+    string
+  ];
+  const description: Metadata["description"] = MARKET;
+  const title: Metadata["title"] = getPageTitle([MARKET]);
+
+  return {
+    description,
+    title,
+  };
+}
+
+export default async function Page({ params }: MarketPageProps) {
+  const { slug = [] } = (await params) || {};
+  const [slugMarketType, ...restSlug] = slug.map((s) => s?.toLowerCase()) as [
+    MarketType,
+    string
+  ];
+
+  if (restSlug.length) {
+    notFound();
+  }
+  return <MarketPage />;
+}
