cc90cb576aea08f82357e0ed917b6732f5cf1bae
nifanic <nifanic@masterpage.net>
2026-02-15 14:42:46 -0600

    refactor(ReturnsLast): :recycle: Split content into _Chart_ and _Legend_
    OPTA-36

--- src/features/dashboard/Dashboard/widgets/funds/ReturnsLast/component.tsx
+++ src/features/dashboard/Dashboard/widgets/funds/ReturnsLast/component.tsx
@@ -1,11 +1,26 @@
+import { Grid } from "@mui/material";
+
 import { Placeholder, Widget, WidgetProps } from "@/components";
 
 export function ReturnsLast(props: Omit<WidgetProps, "title">) {
   return (
     <Widget {...props} title="Returns" subTitle="Last 12mo">
-      <Placeholder sx={{ backgroundColor: "unset", flex: 1 }}>
-        Returns last 12mo
-      </Placeholder>
+      <Grid container columns={4} spacing="10px" sx={{ flex: 1 }}>
+        <Grid
+          component={Placeholder}
+          size={{ md: 3, xs: 4 }}
+          sx={{ height: "unset" }}
+        >
+          Chart
+        </Grid>
+        <Grid
+          component={Placeholder}
+          size={{ md: 1, xs: 4 }}
+          sx={{ height: "unset" }}
+        >
+          Legend
+        </Grid>
+      </Grid>
     </Widget>
   );
 }
